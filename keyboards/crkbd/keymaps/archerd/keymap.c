9d0bfefb921e2de4dcfb103d6045d5d58072572d
David Flores <dflores0818@gmail.com>
2022-04-26 01:48:53 -0600

    Adding leader sequences to print out the current firmware build time for corne layout. (LEADER bd and LEADER bi)

--- keyboards/crkbd/keymaps/archerd/keymap.c
+++ keyboards/crkbd/keymaps/archerd/keymap.c
@@ -140,6 +140,35 @@ void matrix_scan_user(void) {
         SEQ_ONE_KEY(KC_LEAD) {
             SEND_STRING("Crazy thing");
         }
+        // Print firmware build time Apr 26 202201:30:51
+        SEQ_TWO_KEYS(KC_B, KC_D) {
+            SEND_STRING(__DATE__);
+        }
+        SEQ_TWO_KEYS(KC_B, KC_I) {
+            // adapted https://stackoverflow.com/a/64718070/18362972
+            unsigned int compileYear = (__DATE__[7] - '0') * 1000 + (__DATE__[8] - '0') * 100 + (__DATE__[9] - '0') * 10 + (__DATE__[10] - '0');
+            unsigned int compileMonth = (__DATE__[0] == 'J') ? ((__DATE__[1] == 'a') ? 1 : ((__DATE__[2] == 'n') ? 6 : 7))    // Jan, Jun or Jul
+                : (__DATE__[0] == 'F') ? 2                                                              // Feb
+                : (__DATE__[0] == 'M') ? ((__DATE__[2] == 'r') ? 3 : 5)                                 // Mar or May
+                : (__DATE__[0] == 'A') ? ((__DATE__[1] == 'p') ? 4 : 8)                                 // Apr or Aug
+                : (__DATE__[0] == 'S') ? 9                                                              // Sep
+                : (__DATE__[0] == 'O') ? 10                                                             // Oct
+                : (__DATE__[0] == 'N') ? 11                                                             // Nov
+                : (__DATE__[0] == 'D') ? 12                                                             // Dec
+                : 0;
+            unsigned int compileDay = (__DATE__[4] == ' ') ? (__DATE__[5] - '0') : (__DATE__[4] - '0') * 10 + (__DATE__[5] - '0');
+
+            char IsoDate[] =
+            {   compileYear/1000 + '0', (compileYear % 1000)/100 + '0', (compileYear % 100)/10 + '0', compileYear % 10 + '0',
+                '-',  compileMonth/10 + '0', compileMonth%10 + '0',
+                '-',  compileDay/10 + '0', compileDay%10 + '0',
+                0
+            };
+
+            send_string(IsoDate);
+            SEND_STRING("T");
+            SEND_STRING(__TIME__);
+        }
     }
 }
 
