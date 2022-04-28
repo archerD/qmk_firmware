a8f5098cbbe1f534c0275175f0cde512a1b28c81
David Flores <dflores0818@gmail.com>
2022-04-27 23:07:03 -0600

    Starting work on kyria configuration. Still need to setup the keymap, but configuration should be good.

---
+++ keyboards/splitkb/kyria/keymaps/archerd/rules.mk
@@ -0,0 +1,34 @@
+# set default bootloader
+BOOTLOADER = qmk-dfu
+
+# enabled featuers
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+MOUSEKEY_ENABLE = yes # Mouse keys
+EXTRAKEY_ENABLE = yes # Audio control and System control
+LEADER_ENABLE = yes
+# SWAP_HANDS_ENABLE = yes
+# WPM_ENABLE = yes
+
+# rgb stuff
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = no
+#RGBLIGHT_ENABLE = no
+#RGB_MATRIX_ENABLE = yes
+
+# some features that I may want to enable
+#BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+#NKRO_ENABLE = yes           # Enable N-Key Rollover
+
+# disabled features and size reductions
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+
+# already disabled features and size reductions
+#BOOTMAGIC_ENABLE = no
+#CONSOLE_ENABLE = no
+#COMMAND_ENABLE = no
+#NKRO_ENABLE = no
+
